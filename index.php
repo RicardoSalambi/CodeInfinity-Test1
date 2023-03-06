15a227ab2d51a0b6e49bcdd209ea2cf21d8d692f
Ricardo Salambi <ricardox200@gmail.com>
2023-03-06 05:12:02 +0200

    Added the fields and buttons for this project

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <div class="header" style=" background-color: rgb(61, 65, 61); color: white">
+            <h2>Proficieny Test 1</h2>
+        </div>
+
+    </head>
+
+    <body>
+        <form>
+            
+            <input type="text" id="name" name="name" placeholder="Name"><br>
+            
+            <input type="text" id="surname" name="surname" placeholder="Surname"><br>
+
+            <input type="number" id="id" name="id" placeholder="ID Number" maxlength="13"><br>
+
+            <input type="text" id="dob" name="dob" placeholder="Date of Birth"><br>
+
+            <button type="submit"> Submit </button>
+
+            <button> Cancel </button>
+            
+
+        </form>
+        
+
+    </body>
+    
+
+
+    
+    
+</html>
