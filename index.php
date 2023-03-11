975c6c8e97dcf1e516e49996c70e5eb57818c179
Ricardo Salambi <ricardox200@gmail.com>
2023-03-11 12:01:40 +0200

    successful capturing of details to Mongodb

--- index.php
+++ index.php
@@ -2,22 +2,57 @@
 
     if (isset($_POST['submit'])) {
 
-        // if (isset(!$_POST['name'])) {
-        //     echo 'Please enter name';  
-        // }
-        // else if (isset(!$_POST['surname'])) {
-        //     echo 'Please enter surname';   
-        // }
-        // else if (isset(!$_POST['id'])) {
-        //     echo 'Please enter id';   
-        // }
-        // else if (isset(!$_POST['dob'])) {
-        //     echo 'Please enter dob';   
-        // }else{
-        //     echo 'Successful Capture of items';
-        // }
+        // connect to mongodb
+        try{
 
-        echo $_POST['name'];
+            $connection = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+            echo "Connection to database successfully <br/>";
+
+        } catch (MongoDBDriverExceptionException $e) {
+
+            echo 'Failed to connect to MongoDB?<br /><br />';
+            echo $e -> getMessage();
+            exit();
+        }	
+
+        // select a database
+        $command = new MongoDB\Driver\Command(['listDatabases' => 1]);
+        try {
+
+            // Excecute command against admin database
+            $result = $connection -> executeCommand('admin', $command);
+            $dbArray = $result -> toArray()[0];
+
+        } catch(MongoDB\Driver\Exception $e) {
+
+            echo $e->getMessage().'<br />';
+            exit;
+        }
+        
+
+        // create CodeInfinity database
+        try {
+            if(!array_search('codeinfinity', array_column( $dbArray -> databases, 'name' ))){
+                
+                echo 'codeinfinity database doesn\'t exist, creating it <br/>';
+
+                $row = new MongoDB\Driver\BulkWrite();
+                $row -> insert( array('dbName'=>'codeinfinity', 'message'=> 'This collection verifys codeinfinity Database creation') );
+                $connection -> executeBulkWrite( 'codeinfinity.createDB', $row );
+                echo 'Created Code Infinity Database <br/>';
+                
+            } 
+        } catch(MongoDB\Driver\Exception $e) {
+            echo $e->getMessage().'<br />';
+        }
+        finally{
+
+            $row = new MongoDB\Driver\BulkWrite();
+            $row -> insert( array( 'Name' => $_POST['name'], 'Surname' => $_POST['surname'],'ID' => $_POST['id'], 'Date of Birth' => $_POST['dob'] ) );
+            $connection -> executeBulkWrite( 'codeinfinity.data', $row );
+
+        }
+        
         
         
     }
@@ -38,22 +73,21 @@
     <body>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
             
-        <div class="fields">
+            <div class="fields">
 
-            <div><input type="text" id="name" name="name" placeholder="Name"></div>
-            
-            <div><input type="text" id="surname" name="surname" placeholder="Surname"></div>
+                <div><input type="text" id="name" name="name" placeholder="Name"></div>
+                
+                <div><input type="text" id="surname" name="surname" placeholder="Surname"></div>
 
-            <div><input type="number" id="id" name="id" placeholder="ID Number" maxlength="13"></div>
+                <div><input type="number" id="id" name="id" placeholder="ID Number" maxlength="13"></div>
 
-            <div><input type="text" id="dob" name="dob" placeholder="Date of Birth"></div>
+                <div><input type="text" id="dob" name="dob" placeholder="Date of Birth"></div>
 
-            <div><button type="submit" name="submit"> Submit </button></div>
+                <div><button type="submit" name="submit"> Submit </button></div>
 
-            <div><button name="cancel"> Cancel </button></div>
+                <div><button name="cancel"> Cancel </button></div>
 
-        </div>
-            
+            </div>
             
 
         </form>
